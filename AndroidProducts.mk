d6a67d8df2d0b3415b4dca9ebd60cb50376e7ae5
officialtechrom <sharmaaniket022@gmail.com>
2022-02-13 21:43:58 +0530

    Onclite: BringUp For KomodoOS12

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_onclite.mk
+    $(LOCAL_DIR)/komodo_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    komodo_onclite-user \
+    komodo_onclite-userdebug \
+    komodo_onclite-eng
